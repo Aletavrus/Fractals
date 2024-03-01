1dca73f73ebc307ab5a85d2e5d90e94d32f2574b
Alex Glybov <alexis.glybov@gmail.com>
2024-03-01 15:56:29 +0300

    First step done

---
+++ Classes/Artist.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+public class Artist
+{
+	private int[][] data;
+	private const double CReal = -0.71;
+	private const double CImaginary = 0.25015;
+	public Bitmap Drawer(int width, int height, int zoom)
+	{
+		int[][] data = new int[width*height][];
+		int index = 0;
+		for (int y  = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+                data[index] = Point(x, y, width, height, zoom);
+                index++;
+            }
+		}
+		Bitmap bitmap = new Bitmap(width, height);
+        for (int i = 0; i < data.Length; i++)
+		{
+            Color color = Colors[data[i][2]];
+			bitmap.SetPixel(data[i][0], data[i][1], color);
+		}
+		return bitmap;
+	}
+
+	private int[] Point(int x, int y, int width, int height, int zoom)
+	{
+        int[] pointParams = new int[3];
+        int iteration = 256;
+        double zX = 1.5 * (x - width / 2) / (0.5 * zoom * width);
+        double zY = 1.0 * (y - height / 2) / (0.5 * zoom * height);
+        while (((Math.Pow(zX, 2) + Math.Pow(zY, 2)) < 4) && iteration > 1)
+        {
+            double temp = Math.Pow(zX, 2) - Math.Pow(zY, 2) + CReal;
+            zY = 2.0 * zX * zY + CImaginary;
+            zX = temp;
+            iteration--;
+        }
+        pointParams[0] = x;
+        pointParams[1] = y;
+        pointParams[2] = iteration;
+        return pointParams;
+    }
+
+    public static Color[] Colors => Enumerable.Range(0, 256)
+    .Select(c => Color.FromArgb((c & 3) * 85 % 256, (c >> 7) * 36 % 256, (c >> 3 & 5) * 36 % 256))
+    .ToArray();
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace Fractals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Artist artist = new Artist();
+            Console.WriteLine("Enter width, height, zoom in 1 string, separated by spaces");
+            string input = Console.ReadLine();
+            string[] stringValues = input.Split();
+            int[] intValues = new int[stringValues.Length];
+            for (int i = 0; i < stringValues.Length; i++)
+            {
+                int value = int.Parse(stringValues[i]);
+                intValues[i] = value;
+            }
+            int width = intValues[0];
+            int height = intValues[1];
+            int zoom = intValues[2];
+            Bitmap bmp = artist.Drawer(width, height, zoom);
+            string path = @"D:\Git Projects\Fractals\Images\Fractal.jpg";
+            bmp.Save(path);
+        }
+    }
+}
